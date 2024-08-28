6af7c13fa5021de65d11223fbe19a6bc047f5f94
mameami26 <mameami26@gmail.com>
2024-08-28 17:42:53 -0400

    push all updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,12 +17,12 @@ module.exports = {
           ? res.status(404).json({ message: 'No user found with that ID' })
           : res.json(user)
       )
-      .catch((err) => res.status(500).json(err));
+      .catch((err) => res.status(400).json(err));
   },
   createUser(req, res) {
     User.create(req.body)
       .then((user) => res.json(user))
-      .catch((err) => res.status(500).json(err));
+      .catch((err) => res.status(400).json(err));
   },
   updateUser(req, res) {
     User.findOneAndUpdate(
--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const user = require('./user');
+const User = require('./User');
 const Thought = require('./Thought');
 
-module.exports = { user, Thought };
+module.exports = { User, Thought };
